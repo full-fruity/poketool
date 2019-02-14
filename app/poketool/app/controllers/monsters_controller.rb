9bd48d680d53117b839ef5a100204a9ffe69abf4
full-furuity <Wc6Wiw3Q4s>
2019-02-14 13:48:24 +0900

    Remove comment out

--- app/poketool/app/controllers/monsters_controller.rb
+++ app/poketool/app/controllers/monsters_controller.rb
@@ -13,8 +13,6 @@ class MonstersController < ApplicationController
     skill3 = Skill.find_by(name: params[:skill3])
     skill4 = Skill.find_by(name: params[:skill4])
 
-    # binding.pry
-
     mymonster = MyMonster.new(
       user_id: current_user.id,
       monster_id: monster.id,
@@ -38,28 +36,6 @@ class MonstersController < ApplicationController
       skill4_id: skill4.id
     )
 
-    # mymonster = MyMonster.new(
-    #   user_id: current_user.id,
-    #   monster_id: 1,
-    #   nicname: params[:nicname],
-    #   parsonality_id: 1,
-    #   individual_hp: params[:individual_hp],
-    #   individual_atk: params[:individual_atk],
-    #   individual_blk: params[:individual_blk],
-    #   individual_ctc: params[:individual_ctc],
-    #   individual_def: params[:individual_def],
-    #   individual_spd: params[:individual_spd],
-    #   effort_hp: params[:effort_hp],
-    #   effort_atk: params[:effort_atk],
-    #   effort_blk: params[:effort_blk],
-    #   effort_ctc: params[:effort_ctc],
-    #   effort_def: params[:effort_def],
-    #   effort_spd: params[:effort_spd],
-    #   skill1: params[:skill1_id],
-    #   skill2: params[:skill2_id],
-    #   skill3: params[:skill3_id],
-    #   skill4: params[:skill4_id]
-    # )
     mymonster.save
     redirect_to('/')
 

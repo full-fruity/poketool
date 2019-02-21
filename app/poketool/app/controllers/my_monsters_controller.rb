7deb228b00fc7376b172bc609154d144939c2c6b
full-furuity <Wc6Wiw3Q4s>
2019-02-21 15:21:59 +0900

    Fix my_monster view

--- app/poketool/app/controllers/my_monsters_controller.rb
+++ app/poketool/app/controllers/my_monsters_controller.rb
@@ -37,7 +37,7 @@ class MyMonstersController < ApplicationController
     )
 
     mymonster.save
-    redirect_to('/my_monster')
+    redirect_to("/my_monster/#{mymonster.id}")
 
   end
 
@@ -45,9 +45,41 @@ class MyMonstersController < ApplicationController
     @monsters = MyMonster.all
   end
 
+  def status_calculation(base, individual, effort, personality)
+    (((base + (individual / 2).floor + (effort / 8).floor) + 5) * personality).floor
+  end
+
   def show
+
     @my_monster = MyMonster.find_by(id: params[:id])
     @monster = @my_monster.monster
+
+    # HP ステータス計算式
+    @my_monster_hp = (@monster.hp + @my_monster.individual_hp / 2 + @my_monster.effort_hp / 8 ) + 60
+
+    #攻撃ステータス計算
+    @my_monster_atk = status_calculation(@monster.atk, @my_monster.individual_atk, @my_monster.effort_atk, @my_monster.parsonality.atk)
+
+    #防御ステータス計算
+    @my_monster_blk = status_calculation(@monster.blk, @my_monster.individual_blk, @my_monster.effort_blk, @my_monster.parsonality.blk)
+
+    #特攻ステータス計算
+    @my_monster_ctc = status_calculation(@monster.ctc, @my_monster.individual_ctc, @my_monster.effort_ctc, @my_monster.parsonality.ctc)
+
+    #特防ステータス計算
+    @my_monster_def = status_calculation(@monster.def, @my_monster.individual_def, @my_monster.effort_def, @my_monster.parsonality.def)
+
+    #素早ステータス計算
+    @my_monster_spd = status_calculation(@monster.spd, @my_monster.individual_spd, @my_monster.effort_spd, @my_monster.parsonality.spd)
+
+    #技検索
+    @my_monster_skill1 = Skill.find_by(id: @my_monster.skill1_id)
+    @my_monster_skill2 = Skill.find_by(id: @my_monster.skill2_id)
+    @my_monster_skill3 = Skill.find_by(id: @my_monster.skill3_id)
+    @my_monster_skill4 = Skill.find_by(id: @my_monster.skill4_id)
+
+    #性格検索
+    @my_monster_personality = Parsonality.find_by(id: @my_monster.parsonality_id)
   end
 
   def destroy

b6f1e746e1fa96e4dcbf3f6cebafb80fba6e6679
full-furuity <Wc6Wiw3Q4s>
2019-02-21 15:19:42 +0900

    Fix appstartapp.dev.sh

--- app/utils/startapp.dev.sh
+++ app/utils/startapp.dev.sh
@@ -2,7 +2,8 @@ bundle install
 
 sh ./utils/wait-for-it.sh db:5432 -t 30
 
-rm /usr/src/app/poketool/tmp/pids/server.pid
+# rm /usr/src/app/poketool/tmp/pids/server.pid
+rm /usr/src/app/poketool/tmp
 
 cd poketool
 bundle install

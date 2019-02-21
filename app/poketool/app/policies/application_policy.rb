439476bdf2d7de9b20be1568334a1934599ae1c6
full-furuity <Wc6Wiw3Q4s>
2019-02-21 15:18:25 +0900

    Fix policy comment

--- app/poketool/app/policies/application_policy.rb
+++ app/poketool/app/policies/application_policy.rb
@@ -2,7 +2,6 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
-    p "initialize!!!"
     @user = user
     @record = record
   end
--- app/poketool/app/policies/my_monster_policy.rb
+++ app/poketool/app/policies/my_monster_policy.rb
@@ -1,14 +1,5 @@
 class MyMonsterPolicy < ApplicationPolicy
   def destroy?
-    p 'ok!!!!!!!!!!!!!!!!!!!!!!!!!!'
-    # true
     record.user_id == user.id
   end
-  # class Scope < Scope
-  #
-  #
-  #   def resolve
-  #     scope.all
-  #   end
-  # end
 end

6c562087b2577ad79d27c44e9eb46e2ba613b352
full-furuity <Wc6Wiw3Q4s>
2019-02-22 17:38:24 +0900

    Create show

--- app/poketool/app/controllers/skills_controller.rb
+++ app/poketool/app/controllers/skills_controller.rb
@@ -2,4 +2,9 @@ class SkillsController < ApplicationController
   def index
     @skills = Skill.order(:screen_name)
   end
+
+  def show
+    @skill = Skill.find_by(name: params[:name])
+  end
+
 end
